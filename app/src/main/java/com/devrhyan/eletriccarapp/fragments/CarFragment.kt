1319fc895bc10571006cf0f3a085af0cbb7af584
oiRhyan <rhyanaraujochaves@gmail.com>
2024-09-13 16:33:11 -0300

    ProgressBar + NetWork Check

--- app/src/main/java/com/devrhyan/eletriccarapp/fragments/CarFragment.kt
+++ app/src/main/java/com/devrhyan/eletriccarapp/fragments/CarFragment.kt
@@ -1,11 +1,14 @@
 package com.devrhyan.eletriccarapp.fragments
 
+import android.app.AlertDialog
 import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -16,9 +19,9 @@ import com.devrhyan.eletriccarapp.models.adapters.CarAdapter
 import com.devrhyan.eletriccarapp.repositorie.CarList
 import com.devrhyan.eletriccarapp.services.RetrofitService
 import kotlinx.coroutines.launch
+import kotlin.system.exitProcess
 
 class CarFragment : Fragment() {
-
 
     private val adapter = CarAdapter()
 
@@ -28,13 +31,23 @@ class CarFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        lifecycleScope.launch {
-            getAllCars()
-        }
-
         val viewer = inflater.inflate(R.layout.car_fragment_layout, container, false)
 
-        val carListViewer : RecyclerView = viewer.findViewById(R.id.rv_cars)
+        val carListViewer: RecyclerView = viewer.findViewById(R.id.rv_cars)
+        val progressBar: ProgressBar = viewer.findViewById(R.id.progressBar)
+
+        lifecycleScope.launch {
+            try {
+                progressBar.visibility = View.VISIBLE
+                getAllCars()
+                progressBar.visibility = View.GONE
+            } catch (e: Exception) {
+                Log.e("API", "Erro na requisição: ${e.message}")
+                if (isAdded) {
+                    showAlertDialog()
+                }
+            }
+        }
 
         carListViewer.adapter = adapter
         carListViewer.layoutManager = LinearLayoutManager(context)
@@ -43,17 +56,25 @@ class CarFragment : Fragment() {
     }
 
     private suspend fun getAllCars() {
-        try {
-            val retrofitClient = RetrofitService.getInstance()
-            val response = retrofitClient.getCars()
+        val retrofitClient = RetrofitService.getInstance()
+        val response = retrofitClient.getCars()
 
-            if(response.isSuccessful) {
-                val body = response.body()
-                adapter.setAllCars(body ?: emptyList())
-                Log.e("API", body.toString())
-            }
-        } catch (e : Exception) {
-            Log.e("API", e.message.toString())
+        if (response.isSuccessful) {
+            val body = response.body()
+            adapter.setAllCars(body ?: emptyList())
+            Log.e("API", body.toString())
+        } else {
+            throw Exception("Falha ao obter os carros: ${response.code()}")
+        }
+    }
+
+    fun showAlertDialog() {
+        if (isAdded) {
+            val alertBuilder = AlertDialog.Builder(requireContext())
+            alertBuilder.setTitle("Erro de conexão")
+            alertBuilder.setMessage("Erro ao conectar-se à internet, verifique sua conexão e tente novamente mais tarde.")
+            alertBuilder.setPositiveButton("OK") { dialog, _ -> exitProcess(0) }
+            alertBuilder.show()
         }
     }
 
